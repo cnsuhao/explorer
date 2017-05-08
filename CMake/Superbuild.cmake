0f1dc79b0b77c34ee8fcfd6a3c07bfa62243fe5b
Jared Vicory <jared.vicory@kitware.com>
2017-05-08 12:01:12 -0400

    Add BioGears to Superbuild.

--- CMake/Superbuild.cmake
+++ CMake/Superbuild.cmake
@@ -12,10 +12,17 @@ ExternalProject_Add(ParaView
 		-DBUILD_TESTING:BOOL=OFF
 )
 
+ExternalPRoject_Add(BioGears
+	GIT_REPOSITORY https://gitlab.kitware.com/biogears/biogears.git
+	BINARY_DIR ${CMAKE_BINARY_DIR}/BioGears-build
+	CMAKE_ARGS
+		-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+)
+
 ExternalProject_Add(BioGearsDemo
 	DEPENDS ParaView
 	SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
-	BINARY_DIR ${CMAKE_BINARY_DIR}
+	BINARY_DIR ${CMAKE_BINARY_DIR}/BioGearsDemo-build
 	INSTALL_COMMAND ""
 	CMAKE_ARGS
 		-DDO_SUPERBUILD:BOOL=OFF
